18a50ecb7beb019ffe74999803ebe821b4e61396
tuanfs <tuanfs.vn@gmail.com>
2022-04-22 07:53:40 +0700

    add button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { Title } from "./components/Title";
+import { Button } from "@mui/material";
 
 export const App = ({ name = "World" }) => {
-  return <Title>Hello {name}! Le Van Tuan</Title>;
+  return (
+    <div>
+      <Title>Hello {name}! Le Van Tuan</Title>
+      <Button variant="contained" sx={{ color: "red" }}>
+        Click
+      </Button>
+    </div>
+  );
 };
