b53257ee491e8a4eb65fc353972f813383cfca04
tuanfs <tuanfs.vn@gmail.com>
2022-04-22 11:16:31 +0700

    test section continue

--- src/App.js
+++ src/App.js
@@ -2,12 +2,30 @@ import React from "react";
 import Banner from "./components/Banner";
 import CardProductItem from "./components/CardProductItem";
 
+const typeComponent = {
+  banner: function (content, index) {
+    return <Banner content={content} key={index} />;
+  },
+  card: function (content, index) {
+    return <CardProductItem content={content} key={index} />;
+  }
+};
+
 export const App = ({ sections = [] }) => {
   return (
     <div>
-      {sections.map((section, item) => (
-        <Banner />
-      ))}
+      {sections.map(section => {
+        section.children = section.children.map(item => {
+          item.children = item.children.map((item, index) => {
+            return (
+              item.component.type !== "" &&
+              typeComponent[item.component.type](item.component.content, index)
+            );
+          });
+          return item;
+        });
+        return section;
+      })}
     </div>
   );
 };
