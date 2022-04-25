797e7bb6627558d42ab1f3f05643e44a5982d052
tuanfs <tuanfs.vn@gmail.com>
2022-04-25 17:57:40 +0700

    fix coupon img

--- src/components/Coupon.js
+++ src/components/Coupon.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Box } from "@mui/material";
 
 export default function Coupon({ section }) {
   return (
     <Box sx={{ display: "flex" }}>
       {section.children.map((item, index) => (
-        <Box key={index}>
+        <Box key={index} sx={{ flex: "1" }}>
           {item.children.map((item, index) => (
             <Box
               key={index}
@@ -16,7 +16,11 @@ export default function Coupon({ section }) {
               }}
             >
               <img
-                sx={{ height: "100%", width: "100%", objectFit: "contain" }}
+                style={{
+                  height: "100%",
+                  width: "95%",
+                  objectFit: "contain"
+                }}
                 src={item.content && item.content.length > 0 && item.content[0]}
                 alt="Banner"
               />
